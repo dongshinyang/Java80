f6a1f4907d81e3796a26c896001dd8a734fe9984
eomjinyoung <jinyoung.eom@gmail.com>
2016-02-22 14:23:12 +0900

    .

---
+++ java01/src/step01/Exam09.java
@@ -0,0 +1,13 @@
+/* 주제: 음수의 표현 */
+package step01;
+
+public class Exam09 {
+  public static void main(String[] args) {
+    System.out.println(Integer.toBinaryString(10));
+    System.out.println(Integer.toBinaryString(-7));
+  }
+}
+
+/*
+
+*/
