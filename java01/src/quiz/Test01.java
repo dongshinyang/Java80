35a4e9f7158cd326c57af7ce48552daea88330db
eomjinyoung <jinyoung.eom@gmail.com>
2016-02-26 17:55:42 +0900

    .

--- java01/src/quiz/Test01.java
+++ java01/src/quiz/Test01.java
@@ -13,16 +13,6 @@ package quiz;
 public class Test01 {
   public static void main(String[] args) {
     int value = 1238694636;
-
-    int result = 0;
-    int n = value;
-    while(n != 0) {
-      if ((n % 2) == 0) {
-        result++;
-      }
-      n /= 10;
-      System.out.println(n);
-    }
-    System.out.println(result);
+    
   }
 }
--- java01/src/step05/Exam16.java
+++ java01/src/step05/Exam16.java
@@ -9,9 +9,4 @@ public class Exam16 {
       }
     }
   }
-
-
-
-
-
 }
