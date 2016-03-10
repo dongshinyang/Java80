d444ae490ba9de7163e23540754139094d5f5e63
eomjinyoung <jinyoung.eom@gmail.com>
2016-03-10 10:36:59 +0900

    .

--- project01/src/bitcamp/pms/ProjectApp.java
+++ project01/src/bitcamp/pms/ProjectApp.java
@@ -1,6 +1,8 @@
 /* 목표
-- 프로젝트 관리시스템 환영 메시지 출력
-  "PMS에 오신 걸 환영합니다."
+- 다섯 명의 회원 정보를 입력 받아 출력하라
+- 사용 문법:
+  => 레퍼런스 배열을 이용하여 여러 인스턴스의 주소를 저장
+  => 반복문 활용
 */
 package bitcamp.pms;
 
@@ -10,21 +12,31 @@ import bitcamp.pms.domain.Member;
 public class ProjectApp {
   public static void main(String[] args) {
     Scanner keyScan = new Scanner(System.in);
-    Member member = new Member();
+    Member[] members = new Member[5];
 
-    System.out.print("이름? ");
-    member.setName(keyScan.nextLine());
+    Member member = null;
+    for (int i = 0; i < members.length; i++) {
+      member = new Member();
 
-    System.out.print("이메일? ");
-    member.setEmail(keyScan.nextLine());
+      System.out.print("이름? ");
+      member.setName(keyScan.nextLine());
 
-    System.out.print("암호? ");
-    member.setPassword(keyScan.nextLine());
+      System.out.print("이메일? ");
+      member.setEmail(keyScan.nextLine());
 
-    System.out.print("전화? ");
-    member.setTel(keyScan.nextLine());
+      System.out.print("암호? ");
+      member.setPassword(keyScan.nextLine());
+
+      System.out.print("전화? ");
+      member.setTel(keyScan.nextLine());
+
+      members[i] = member;
+    }
 
     System.out.println("--------------------------------");
-    System.out.println(member);
+
+    for (int i = 0; i < members.length; i++) {
+      System.out.println(members[i]);
+    }
   }
 }
