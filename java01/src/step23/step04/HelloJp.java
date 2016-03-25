20331aea3a9ed22ead18c17bb443e0a501c5966b
eomjinyoung <jinyoung.eom@gmail.com>
2016-03-25 09:56:53 +0900

    .

--- java01/src/step23/step04/HelloJp.java
+++ java01/src/step23/step04/HelloJp.java
@@ -10,6 +10,14 @@ public class HelloJp extends AbstractHello {
     list.add("こんにちは。");
     list.add("こんばんは");
   }
+
+  @Override
+  public String greet() {
+    return sayHello();
+  }
+
+  // 이건 기존에 이미 사용한 경우를 대비해서 제거하지 않았음.
+  // 나 실력없는 사람 아님!
   public String sayHello() {
     Calendar cal = Calendar.getInstance();
     int hour = cal.get(Calendar.HOUR);
