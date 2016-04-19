994046cdca2fdcf21ccaa4ea9619874d7a4abe94
'Jinyoung' <'jinyoung.eom@gmail.com'>
2016-04-19 16:36:58 +0900

    .

--- java01/src/step28/project.sql
+++ java01/src/step28/project.sql
@@ -1,3 +1,13 @@
+create table BOARDS (
+  BNO int primary key auto_increment,
+  TITLE varchar(200) not null,
+  CONTS text not null,
+  VWCNT int default 0,
+  PWD varchar(10),
+  CDT datetime
+);
+
+
 create table PROJECTS (
   PNO     int primary key auto_increment,
   TITLE   varchar(200) not null,
