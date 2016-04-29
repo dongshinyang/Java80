00975a901996c13e791e9fe1423d90389d5f4278
'Jinyoung' <'jinyoung.eom@gmail.com'>
2016-04-29 14:52:58 +0900

    .

--- project01/src/bitcamp/pms/controller/MemberController.java
+++ project01/src/bitcamp/pms/controller/MemberController.java
@@ -4,7 +4,6 @@ import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import bitcamp.pms.annotation.RequestMapping;
 import bitcamp.pms.dao.MemberDao;
 import bitcamp.pms.domain.Member;
-import bitcamp.pms.util.CommandUtil;
 
 @Controller
 @RequestMapping("member/")
@@ -37,24 +35,19 @@ public class MemberController {
   }
   
   @RequestMapping("delete.do")
-  public void delete(Scanner keyScan) {
+  public void delete(Map<String,String> paramMap, PrintStream out) {
     try {
-      System.out.print("삭제할 회원 번호는? ");
-      int no = Integer.parseInt(keyScan.nextLine());
-  
-      if (CommandUtil.confirm(keyScan, "정말 삭제하시겠습니까?")) {
-        int count = memberDao.delete(no);
-        if (count > 0) {
-          System.out.println("삭제하였습니다.");
-        } else {
-          System.out.println("유효하지 않은 번호이거나, 이미 삭제된 항목입니다.");
-        }
+      int no = Integer.parseInt(paramMap.get("no"));
+      int count = memberDao.delete(no);
+      
+      if (count > 0) {
+        out.println("삭제하였습니다.");
       } else {
-        System.out.println("삭제를 취소하였습니다.");
+        out.println("유효하지 않은 번호이거나, 이미 삭제된 항목입니다.");
       }
       
     } catch (Exception e) {
-      System.out.println("데이터 처리에 실패했습니다.");
+      out.println("데이터 처리에 실패했습니다.");
     }
   }
   
@@ -74,44 +67,35 @@ public class MemberController {
   }
   
   @RequestMapping("update.do")
-  public void update(Scanner keyScan) {
+  public void update(Map<String,String> paramMap, PrintStream out) {
     try {
-      System.out.print("변경할 회원 번호는? ");
-      int no = Integer.parseInt(keyScan.nextLine());
+      int no = Integer.parseInt(paramMap.get("no"));
   
-      HashMap<String,Object> paramMap = new HashMap<>();
-      paramMap.put("no", no);
+      HashMap<String,Object> sqlParamMap = new HashMap<>();
+      sqlParamMap.put("no", no);
       
-      Member member = memberDao.selectOne(paramMap);
+      Member member = memberDao.selectOne(sqlParamMap);
       if (member == null) {
-        System.out.println("유효하지 않은 번호입니다.");
+        out.println("유효하지 않은 번호입니다.");
         return;
       }
       
-      System.out.printf("이름(%s)? ", member.getName());
-      member.setName(keyScan.nextLine());
-  
-      System.out.printf("이메일(%s)? ", member.getEmail());
-      member.setEmail(keyScan.nextLine());
-  
-      System.out.printf("암호(%s)? ", member.getPassword());
-      member.setPassword(keyScan.nextLine());
-  
-      System.out.printf("전화(%s)? ", member.getTel());
-      member.setTel(keyScan.nextLine());
-  
-      if (CommandUtil.confirm(keyScan, "변경하시겠습니까?")) {
-        int count = memberDao.update(member);
-        if (count > 0) {
-          System.out.println("변경하였습니다.");
-        } else {
-          System.out.println("유효하지 않은 번호이거나, 이미 삭제된 항목입니다.");
-        }
+      member = new Member();
+      member.setNo(no);
+      member.setName(paramMap.get("name"));
+      member.setEmail(paramMap.get("email"));
+      member.setPassword(paramMap.get("password"));
+      member.setTel(paramMap.get("tel"));
+      
+      int count = memberDao.update(member);
+      
+      if (count > 0) {
+        out.println("변경하였습니다.");
       } else {
-        System.out.println("변경을 취소하였습니다.");
+        out.println("유효하지 않은 번호이거나, 이미 삭제된 항목입니다.");
       }
     } catch (Exception e) {
-      System.out.println("데이터 처리에 실패했습니다.");
+      out.println("데이터 처리에 실패했습니다.");
     }
   }
 }
