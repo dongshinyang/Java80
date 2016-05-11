1eb33589cd67626810d3c0cd2cacd5602b8ac092
'Jinyoung' <'jinyoung.eom@gmail.com'>
2016-05-11 12:58:56 +0900

    .

--- web03/README.md
+++ web03/README.md
@@ -17,12 +17,36 @@ src/main/java         => 자바 소스 파일을 두는 폴더
 - 서블릿에서 공통으로 사용할 스프링 IoC 컨테이너를 준비한다.
 1) "bitcamp.pms.listener" 패키지 생성
 2) ServletContextListener 구현체 "ContextLoaderListener" 클래스 생성
-2) web.xml에 리스너 배치 설정
+3) web.xml에 리스너 배치 설정
    - src/main/webapp/WEB-INF 폴더 추가
    - web.xml 파일 준비(기존 프로젝트의 파일을 가져온다)
    - web.xml 파일을 현재 프로젝트에 맞게끔 초기화시킨다.
    - 리스너 등록
-3) 톰캣 서버에 추가한 후 실행 테스트    
+  <listener>
+    <listener-class>bitcamp.pms.listener.ContextLoaderListener</listener-class>
+  </listener>
+4) 톰캣 서버에 추가한 후 실행 테스트    
+5) 스프링 설정 파일 준비
+   - src/main/webapp/WEB-INF/conf 폴더 추가
+   - 스프링 설정 파일 준비(project01에서 가져온다)
+     WEB-INF/conf/application-context.xml
+   - 스프링 설정 파일을 현재 프로젝트에 맞게 변경한다.
+6) 스프링 설정 파일의 위치 정보를 컨텍스트 파라미터로 등록한다.
+   - web.xml에 등록
+  <context-param>
+    <param-name>contextConfigLocation</param-name>
+    <param-value>/WEB-INF/conf/application-context.xml</param-value>
+  </context-param>
+7) 리스너에서 스프링 IoC 객체 생성하기
+   - ContextLoaderListener.contextInitialized() 메서드 편집
+   
+  
+  
+
+
+
+
+
 
 
 
--- web03/src/main/java/bitcamp/pms/listener/ContextLoaderListener.java
+++ web03/src/main/java/bitcamp/pms/listener/ContextLoaderListener.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class ContextLoaderListener implements ServletContextListener {
   
@@ -15,11 +15,13 @@ public class ContextLoaderListener implements ServletContextListener {
     // 1) 스프링 설정 파일의 절대 경로 알아내기
     ServletContext servletContext = sce.getServletContext();
     String configPath = servletContext.getInitParameter("contextConfigLocation");
-    System.out.println(servletContext.getRealPath(configPath));
     
-    //ApplicationContext applicationContext = 
-    //    new ClassPathXmlApplicationContext();
+    // 2) 스프링 설정 파일의 경로를 가지고 IoC 컨테이너 객체를 준비한다.
+    ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
+        servletContext.getRealPath(configPath));
     
+    // 3) 모든 서블릿이 스프링 IoC 컨테이너를 사용할 수 있도록 바구니에 저장한다.
+    servletContext.setAttribute("iocContainer", applicationContext);
   }
 
   @Override
