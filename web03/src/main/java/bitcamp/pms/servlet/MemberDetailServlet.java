a5a465e1ead94e9628185c84458a7f64cd9756e8
'Jinyoung' <'jinyoung.eom@gmail.com'>
2016-05-12 11:43:43 +0900

    .

--- web03/src/main/java/bitcamp/pms/servlet/MemberDetailServlet.java
+++ web03/src/main/java/bitcamp/pms/servlet/MemberDetailServlet.java
@@ -2,7 +2,8 @@ package bitcamp.pms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -16,21 +17,24 @@ import org.springframework.context.ApplicationContext;
 import bitcamp.pms.dao.MemberDao;
 import bitcamp.pms.vo.Member;
 
-@WebServlet("/member/list.do")
-public class MemberListServlet extends HttpServlet {
+@WebServlet("/member/detail.do")
+public class MemberDetailServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
   protected void doGet(
       HttpServletRequest request, 
       HttpServletResponse response) throws ServletException, IOException {
+    
     ServletContext servletContext = this.getServletContext();
     ApplicationContext iocContainer = 
         (ApplicationContext)servletContext.getAttribute("iocContainer");
-    
     MemberDao memberDao = iocContainer.getBean(MemberDao.class);
     
-    List<Member> list = memberDao.selectList();
+    Map<String,Object> paramMap = new HashMap<>();
+    paramMap.put("no", Integer.parseInt(request.getParameter("no")));
+    
+    Member member = memberDao.selectOne(paramMap);
     
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
@@ -40,27 +44,21 @@ public class MemberListServlet extends HttpServlet {
     out.println("<title>회원관리</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>회원-목록</h1>");
-    out.println("<p><a href='add.do'>새 회원</a></p>");
-    out.println("<table border='1'>");
-    out.println("<thead>");
-    out.println("<tr>");
-    out.println("  <th>번호</th>");
-    out.println("  <th>이름</th>");
-    out.println("  <th>이메일</th>");
-    out.println("  <th>전화</th>");
-    out.println("</tr>");
-    out.println("</thead>");
-    out.println("<tbody>");
-    for (Member member : list) {
-      out.printf("<tr><td>%d</td>", member.getNo());
-      out.printf("<td><a href='detail.do?no=%d'>%s</a></td>", 
-          member.getNo(), member.getName());
-      out.printf("<td>%s</td>", member.getEmail());
-      out.printf("<td>%s</td></tr>\n", member.getTel());
-    }
-    out.println("</tbody>");
-    out.println("</table>");
+    out.println("<h1>회원관리-상세정보</h1>");
+    out.println("<form action='update.do' method='post'>");
+    out.printf("번호: <input type='text' name='no' value='%d' readonly><br>\n",
+        member.getNo());
+    out.printf("이름: <input type='text' name='name' value='%s'><br>\n",
+        member.getName());
+    out.printf("이메일: <input type='text' name='email' value='%s'><br>\n",
+        member.getEmail());
+    out.println("암호: <input type='password' name='password'><br>");
+    out.printf("전화: <input type='text' name='tel' value='%s'><br>\n",
+        member.getTel());
+    out.println("<button>변경</button>");
+    out.printf("<a href='delete.do?no=%d'>삭제</a>", member.getNo());
+    out.println("<button type='reset'>초기화</button>");
+    out.println("</form>");
     out.println("</body>");
     out.println("</html>");
   }
