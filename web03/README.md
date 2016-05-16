329b3177e75713c46306d7ee69e6c4de87abc9c9
'Jinyoung' <'jinyoung.eom@gmail.com'>
2016-05-16 16:39:31 +0900

    .

--- web03/README.md
+++ web03/README.md
@@ -104,6 +104,12 @@ src/main/java         => 자바 소스 파일을 두는 폴더
    - MemberUpdateServlet --> MemberUpdateController
    - MemberDeleteServlet --> MemberDeleteController    
 
+# CRUD 페이지 컨트롤러들을 한 클래스로 합치기(src05)
+1) BoardXxxController를 BoardController로 합치기
+   - BoardController 클래스를 작성한다.
+   - 각 BoardXxxController의 execute()를 가져와서 이름을 바꾼다.
+   - 요청을 처리하는 각 메서드에 @RequestMapping 애노테이션을 붙인다.
+   
 
 
 
--- web03/src/main/java/bitcamp/pms/servlet/DispatcherServlet.java
+++ web03/src/main/java/bitcamp/pms/servlet/DispatcherServlet.java
@@ -2,6 +2,7 @@ package bitcamp.pms.servlet;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 public class DispatcherServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -26,16 +28,11 @@ public class DispatcherServlet extends HttpServlet {
     String pageControllerPath = request.getServletPath().replace(".do", "");
     
     // 2) 페이지 컨트롤러를 찾는다.
-    ServletContext servletContext = this.getServletContext();
-    ApplicationContext applicationContext = 
-        (ApplicationContext)servletContext.getAttribute("iocContainer");
-    Object pageController = applicationContext.getBean(pageControllerPath);
+    Object pageController = findPageController(pageControllerPath);
     
     try {
-      // 3) 페이지 컨트롤러의 execute() 메서드를 호출한다.
-      Method method = pageController.getClass().getMethod("execute", 
-                            HttpServletRequest.class,
-                            HttpServletResponse.class);
+      // 3) 페이지 컨트롤러에서 해당 URL을 처리하는 메서드를 찾는다.
+      Method method = findRequestHandler(pageController, pageControllerPath);
       String viewUrl = (String)method.invoke(pageController, request, response);
     
       // 4) 뷰 컴포넌트를 실행한다.
@@ -48,6 +45,51 @@ public class DispatcherServlet extends HttpServlet {
     } catch (Exception e) {
       throw new ServletException(e);
     }
+  }
+
+  private Method findRequestHandler(Object pageController, String pageControllerPath) {
+    // 1) 페이지 컨트롤러의 클래스 정보를 알아낸다.
+    Class<?> clazz = pageController.getClass();
+    
+    // 2) 클래스에 붙은 @RequestMapping의 값을 알아내기
+    RequestMapping anno = clazz.getAnnotation(RequestMapping.class);
+    String url = anno.value()[0];
+    
+    // 2) 클래스에 선언된 메서드 목록을 알아낸다.
+    Method[] methods = clazz.getMethods();
+    
+    // 3) @RequestMapping이 붙은 메서드를 알아낸다.
+    for (Method m : methods) {
+      anno = m.getAnnotation(RequestMapping.class);
+      if (anno == null) 
+        continue;
+      
+      // 페이지 컨트롤러에 선언한 경로와 메서드에 선언한 경로를 합쳐서
+      // pageControllerPath와 같은 지 비교한다.
+      if ((url + anno.value()[0]).equals(pageControllerPath))
+        return m;
+    }
+    
+    return null;
+  }
+
+  private Object findPageController(String pageControllerPath) {
+    ServletContext servletContext = this.getServletContext();
+    ApplicationContext applicationContext = 
+        (ApplicationContext)servletContext.getAttribute("iocContainer");
+    
+    // 1) @RequestMapping이 붙은 객체를 찾는다.
+    Map<String,Object> beanMap = 
+        applicationContext.getBeansWithAnnotation(RequestMapping.class);
+    
+    // 2) 애노테이션의 값이 pageControllerPath와 앞부분이 일치하는 지 검사한다.
+    String value = null;
+    for (Object obj : beanMap.values()) {
+      value = obj.getClass().getAnnotation(RequestMapping.class).value()[0]; 
+      if (pageControllerPath.startsWith(value))
+        return obj;
+    }
+    return null;
   }
 }
 
